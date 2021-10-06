b68db21580876a1b60f82c954061baeae2bf8909
Jankorepo <44462192+Jankorepo@users.noreply.github.com>
2021-10-06 13:42:56 +0200

    commit

--- Program.cs
+++ Program.cs
@@ -12,23 +12,23 @@ namespace ConvolutionNeuralNetwork
         static void Main(string[] args)
         {
             Web neural_web = new Web();
-            neural_web.web_structure = new List<int>() { 2,2,1 };
+            neural_web.web_structure = new List<int>() { 784,16,16,10 };
             neural_web.FillSetValues(neural_web);
             double learningRate = 0.1;
-            //List<InputsAndOutputs> listOfTrainingImages = Mnist.GetImagesFromFile(@"mnist_784_csv.csv", neural_web.web_structure);
-            List<InputsAndOutputs> listOfTrainingImages = new List<InputsAndOutputs>() { new InputsAndOutputs(new List<double>() { 1, 0 }, new List<int>() { 1 }) };
+            List<InputsAndOutputs> listOfTrainingImages = Mnist.GetImagesFromFile(@"mnist_784_csv.csv", neural_web.web_structure);
+            
 
             //poprawić filtry?
 
             double goodAnswers = 0;
             var rand = new Random();
-            int epochs = 100;
+            int epochs = 70000;
             for (int i = 0; i < epochs; i++)
             {
                 var choosenImage1= listOfTrainingImages[rand.Next(listOfTrainingImages.Count - 1)];
                 //var imageIn2DArray1 = EditImage.Swap1DListTo2DList(choosenImage1.Inputs, 28);
                 //List<List<List<double>>> listOfConvImages1 = new List<List<List<double>>>();
-                
+
                 //for (int x = 0; x < EditImage.GetSome3x3Filters().Count; x++)
                 //{
                 //    var convImage = EditImage.Convolution(imageIn2DArray1, EditImage.GetSome3x3Filters()[x], stride: 1);
@@ -39,11 +39,25 @@ namespace ConvolutionNeuralNetwork
                 //}
                 //var inputs1=EditImage.SwapAll2DListsTo1DList(listOfConvImages1);
 
-                
-
-
                 CalculateWebData.Calculate_Output(neural_web, choosenImage1.Inputs);
                 CalculateWebData.BackwardPropagation(neural_web, choosenImage1.Outputs, learningRate);
+                int num1 = 0;
+                int num2 = choosenImage1.Outputs.IndexOf(choosenImage1.Outputs.Where(x => x == 1).FirstOrDefault());
+                double max = 0;
+                foreach (var item in neural_web.layers.Last())
+                {
+                    if (item.output > max)
+                    {
+                        num1 = neural_web.layers.Last().IndexOf(item);
+                        max = item.output;
+                    }
+
+                }
+                if (i > 69900)
+                    if (num1 == num2)
+                        goodAnswers++;
+
+
 
 
             }
@@ -56,6 +70,9 @@ namespace ConvolutionNeuralNetwork
             //bitmap.Save("mnist" + 5 + ".png", ImageFormat.Png);
 
             Console.ReadKey();
+
+            
         }
+
     }
 }
